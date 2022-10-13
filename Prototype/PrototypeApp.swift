c8a71ad5164a4b898ab14cdd9934b810e10e012a
phatrapol-tin <61874603+phatrapol-tin@users.noreply.github.com>
2022-10-13 12:13:59 +0800

    Initial Commit

---
+++ Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Phatrapol Tinmanee on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
